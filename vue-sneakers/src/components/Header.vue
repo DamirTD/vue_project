11d85502f6b8cd4cd70784f94fbfbc4c68d28623
DamirTD <toriya.damir.2004@gmail.com>
2024-01-25 17:47:52 +0600

    finally done

---
+++ vue-sneakers/src/components/Header.vue
@@ -0,0 +1,42 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+const emit = defineEmits(['openDrawer'])
+
+defineProps({
+  totalPrice: Number
+})
+</script>
+
+<template>
+  <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+    <router-link to="/">
+      <div class="flex items-center gap-4">
+        <img src="/logo.png" alt="Logo" class="w-10" />
+        <div>
+          <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+          <p class="text-slate-400">Магазин лучших кроссовок</p>
+        </div>
+      </div>
+    </router-link>
+    <ul class="flex items-center gap-10">
+      <li
+        @click="() => emit('openDrawer')"
+        class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black"
+      >
+        <img src="/cart.svg" alt="Cart" />
+        <b>{{ totalPrice }} kzt</b>
+      </li>
+      <router-link to="/favorites">
+        <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+          <img src="/heart.svg" alt="Likes" />
+          <span>Закладки</span>
+        </li>
+      </router-link>
+
+      <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+        <img src="/profile.svg" alt="Profile" />
+        <span>Профиль</span>
+      </li>
+    </ul>
+  </header>
+</template>
